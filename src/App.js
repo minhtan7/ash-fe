802c60cf9602fa76aad5593461c18b3cd4d48d69
Emily <113935577+WeiEmily@users.noreply.github.com>
2023-04-08 22:33:04 +0100

    add some comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+// Importing useEffect hook from the React library
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+// Importing React Router libraries
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-
-
+// Importing page components
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -11,29 +12,30 @@ import Home from "./pages/Home"
 import MainLayout from './component/layout';
 
 function App() {
-  useEffect(()=>{
+  // Using the useEffect hook to fetch data from the server on component mount
+  useEffect(() => {
     fetch("http://localhost:5000/api/cards")
   })
-
-  useEffect(()=>{
-
-  },[])
-
+  // Using the useEffect hook with an empty dependency array to run code only once on component mount
+  useEffect(() => {
+  }, [])
+  // Returning the component tree
   return (
     <AuthProvider>
-     <Router>
-      <Routes>
-        <Route path='/' element={<MainLayout/>}>
-          <Route path='/' element={<Dashboard />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/register' element={<Register />}/>
-          <Route path='/signed' element={<Home />}/>
-        </Route>
-      </Routes>
-    </Router>
+      {/* Wrapping the component tree in the AuthProvider context provider*/}
+      <Router>
+        <Routes>
+          <Route path='/' element={<MainLayout />}>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/signed' element={<Home />} />
+          </Route>
+        </Routes>
+      </Router>
 
     </AuthProvider>
   );
 }
-
+// Exporting the App component
 export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,67 +2,69 @@ import { useState, useEffect } from "react"
 import { FaUser } from "react-icons/fa";
 
 function Register() {
+    // Define the initial state using the useState hook
     const [formData, setFormData] = useState({
-        name:'',
-        email:'',
-        password:'',
-        password2:''
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
     })
-
-    const {name, email, password, password2} = formData;
-
+    // Extract variables from the formData state
+    const { name, email, password, password2 } = formData;
+    // Define the onChange event handler to update the formData state
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
             [e.target.name]: e.target.value,
         }))
     }
+    // Define the onSubmit event handler to handle form submission
     const onSubmit = (e) => {
         e.preventDefault()
     }
+    // Render the Register component
+    return <div className="container">
+        <section className="heading">
+            <h1>
+                <FaUser />Register
+            </h1>
+            <p>Please create an account</p>
+        </section>
 
-  return <div className="container">
-  <section className="heading">
-    <h1>
-        <FaUser />Register
-    </h1>
-    <p>Please create an account</p>
-  </section>
+        <section className="form">
+            <form onSubmit={onSubmit}></form>
+            <form >
+                <div className="form-group">
+                    <input type="text" className="form-control" id="name"
+                        name="name" value={name} placeholder='Enter your name'
+                        onChange={onChange} />
+                </div>
 
-  <section className="form">
-    <form onSubmit={onSubmit}></form>
-    <form >
-        <div className="form-group">
-            <input type="text" className="form-control" id="name"
-            name="name" value={name} placeholder='Enter your name'
-            onChange={onChange}/>
-        </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" id="email"
+                        name="email" value={email} placeholder='Enter your email'
+                        onChange={onChange} />
+                </div>
 
-        <div className="form-group">
-            <input type="text" className="form-control" id="email"
-            name="email" value={email} placeholder='Enter your email'
-            onChange={onChange}/>
-        </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" id="password"
+                        name="password" value={password} placeholder='Enter your password'
+                        onChange={onChange} />
+                </div>
 
-        <div className="form-group">
-            <input type="text" className="form-control" id="password"
-            name="password" value={password} placeholder='Enter your password'
-            onChange={onChange}/>
-        </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" id="password2"
+                        name="password2" value={password2} placeholder='retype your password'
+                        onChange={onChange} />
+                </div>
 
-        <div className="form-group">
-            <input type="text" className="form-control" id="password2"
-            name="password2" value={password2} placeholder='retype your password'
-            onChange={onChange}/>
-        </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-block">submit</button>
+                </div>
 
-        <div className="form-group">
-            <button type="submit" className="btn btn-block">submit</button>
-        </div>
-        
-    </form>
-  </section>
-  </div>
+            </form>
+        </section>
+    </div>
 }
 
 export default Register
