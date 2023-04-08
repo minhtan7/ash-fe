703cce96130056fa8d296b6202d22454a14ee249
Emily <113935577+WeiEmily@users.noreply.github.com>
2023-04-08 22:20:43 +0100

    fix problem of logout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,20 +7,7 @@ import cardData from './cardData.json'
 function Dashboard() {
   return (
 
-    <html lang="en">
-      <head>
-        <title>Bootstrap Example</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-        />
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-          defer
-        ></script>
-      </head>
+   
     
       <div className="container">
         <div className="row">
@@ -57,7 +44,6 @@ function Dashboard() {
         </div>
         
       </div>
-      </html>
     );
   
 }
