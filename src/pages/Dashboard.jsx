67e1a94029bf7a444e45a1bcecae107e1d501cf0
unknown <douglasd158@gmail.com>
2023-04-10 22:24:33 +0100

    Added basic Deck Management

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
+import './Dashboard.css'
+import cardData from './cardData.json'
+
+
 
 function Dashboard() {
   return (
-    <div className='container'>Dashboard</div>
-  )
+
+    <html lang="en">
+      <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+          defer
+        ></script>
+      </head>
+    
+      <div className="container">
+        <div className="row">
+          <h1>
+            <center> Card/Deck Management </center>
+          </h1>
+        </div>
+        <div className="row mb-3">
+          {cardData.slice(0, 3).map((card, index) => (
+            <div className="col-sm-4" key={index}>
+              <div className="card">
+                <img src={card.image} alt={card.name} />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="row mb-3">
+          {cardData.slice(3, 6).map((card, index) => (
+            <div className="col-sm-4" key={index}>
+              <div className="card">
+                <img src={card.image} alt={card.name} />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="row mb-3">
+          {cardData.slice(6, 9).map((card, index) => (
+            <div className="col-sm-4" key={index}>
+              <div className="card">
+                <img src={card.image} alt={card.name} />
+              </div>
+            </div>
+          ))}
+        </div>
+        
+      </div>
+      </html>
+    );
+  
 }
 
 export default Dashboard
