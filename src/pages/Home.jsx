ad2ae7dce34690716d5b81d2eee24c57c50f3d41
Emily <113935577+WeiEmily@users.noreply.github.com>
2023-04-24 15:42:38 +0100

    just puch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,38 @@
 
+import "./resourse/HomeResouce/styles.css"; //styles.css
+import FireVideo from "./resourse/HomeResouce/Fire.mp4";
+import CardsSound from "./resourse/HomeResouce/cards.mp3";
+import SwordSound from "./resourse/HomeResouce/sword.mp3";
+import CracklingSound from "./resourse/HomeResouce/Crackling.mp3";
+import KeyIcon from "./resourse/HomeResouce/key.svg";
 
-import FireVideo from "./resourse/Fire.mp4";//../Fire.mp4
-import CardsSound from "./resourse/cards.mp3";
-import SwordSound from "./resourse/sword.mp3";
-//import CracklingSound from "../Crackling.mp3";
-import KeyIcon from "./resourse/key.svg";
-import { Link } from 'react-router-dom'
+function Home() {
+    const playSound = (soundUrl) => {
+        const audio = new Audio(soundUrl);
+        audio.play();
+    };
+    playSound(CracklingSound);
 
-const Home = () => {
-  const playSound = (soundUrl) => {
-    const audio = new Audio(soundUrl);
-    audio.play();
-  };
-  //playSound(CracklingSound);
-
-  return (
-    <div className="App">
-      <h1 className="title">EMPIRES ASCENSION</h1>
-      <div className="bg-container">
-        <video className="bg" autoPlay muted loop>
-          <source src={FireVideo} type="video/mp4" />
-        </video>
-      </div>
-      <Link to='/play'>
-        <button className="button button1" onClick={() => playSound(SwordSound)}>
-          PLAY
-        </button>
-      </Link>
-      <Link to='/Dashboard'>
-        <button className="button button2" onClick={() =>
-          playSound(CardsSound)
-
-        }>
-          DECKS
-        </button>
-      </Link>
-      <button className="button button3">PACK OPENING</button>
-      <Link to='/Login'>
-        <button className="button button4">
-          <img src={KeyIcon} alt="Login" /> LOGIN
-        </button>
-      </Link>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>EMPIRES ASCENSION</h1>
+            <div className="bg-container">
+                <video className="bg" autoPlay muted loop>
+                    <source src={FireVideo} type="video/mp4" />
+                </video>
+            </div>
+            <button className="button button1" onClick={() => playSound(SwordSound)}>
+                PLAY
+            </button>
+            <button className="button button2" onClick={() => playSound(CardsSound)}>
+                DECKS
+            </button>
+            <button className="button button3">SETTINGS</button>
+            <button className="button button4">
+                <img src={KeyIcon} alt="Login" /> LOGIN
+            </button>
+        </div>
+    );
 }
 
 export default Home
