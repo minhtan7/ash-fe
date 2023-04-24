af1c99e6720213fd2609923ea76fd8a2c2a498e7
Emily <113935577+WeiEmily@users.noreply.github.com>
2023-04-24 15:42:38 +0100

    just puch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,38 @@
-import { useState, useEffect } from "react"
-import { FaUser } from "react-icons/fa";
 
-const Home = ()=>{
+import "./resourse/HomeResouce/styles.css"; //styles.css
+import FireVideo from "./resourse/HomeResouce/Fire.mp4";
+import CardsSound from "./resourse/HomeResouce/cards.mp3";
+import SwordSound from "./resourse/HomeResouce/sword.mp3";
+import CracklingSound from "./resourse/HomeResouce/Crackling.mp3";
+import KeyIcon from "./resourse/HomeResouce/key.svg";
+
+function Home() {
+    const playSound = (soundUrl) => {
+        const audio = new Audio(soundUrl);
+        audio.play();
+    };
+    playSound(CracklingSound);
+
     return (
-        <div className="container">
-        <h1>home</h1>
+        <div className="App">
+            <h1>EMPIRES ASCENSION</h1>
+            <div className="bg-container">
+                <video className="bg" autoPlay muted loop>
+                    <source src={FireVideo} type="video/mp4" />
+                </video>
+            </div>
+            <button className="button button1" onClick={() => playSound(SwordSound)}>
+                PLAY
+            </button>
+            <button className="button button2" onClick={() => playSound(CardsSound)}>
+                DECKS
+            </button>
+            <button className="button button3">SETTINGS</button>
+            <button className="button button4">
+                <img src={KeyIcon} alt="Login" /> LOGIN
+            </button>
         </div>
-    )
+    );
 }
 
 export default Home
